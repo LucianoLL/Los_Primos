9340f1e42285197446d6fe81fbeedc2dec2b67e6
Michael Alaniz <56569472+SpookyAl@users.noreply.github.com>
2023-02-05 06:31:51 -0800

    image thing idk

---
+++ TWR_AR/Assets/Scripts/Track.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+public class Track : MonoBehaviour
+{
+    ARTrackedImageManager image;
+    public GameObject ok;
+
+    void Awake()
+    {
+        image = GetComponent<ARTrackedImageManager>();       
+    }
+
+    private void OnEnable()
+    {
+        image.trackedImagesChanged += OnTrackedChanged;
+    }
+
+    private void OnDisable()
+    {
+        image.trackedImagesChanged -= OnTrackedChanged;
+    }
+
+    void OnTrackedChanged(ARTrackedImagesChangedEventArgs spawnArgs) {
+
+        foreach (var spawnSymbol in spawnArgs.added) {
+            Debug.Log(spawnSymbol.name);   
+        }
+
+    }
+
+}
