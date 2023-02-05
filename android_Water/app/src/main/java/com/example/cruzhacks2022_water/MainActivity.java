260a73cac53f79d570c1c39a4e5138c3d722a340
Luciano Loma Lorenzana <lucianolomal@gmail.com>
2023-02-05 06:45:15 -0800

    Pushing current android assets, in case computer crashes

--- android_Water/app/src/main/java/com/example/cruzhacks2022_water/MainActivity.java
+++ android_Water/app/src/main/java/com/example/cruzhacks2022_water/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.cruzhacks2022_water;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggleButton;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
