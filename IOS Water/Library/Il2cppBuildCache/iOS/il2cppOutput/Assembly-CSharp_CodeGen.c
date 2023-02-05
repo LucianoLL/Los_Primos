d98f5132c811f70d071e64f751dde9f7fa973e5f
Michael Alaniz <56569472+SpookyAl@users.noreply.github.com>
2023-02-04 21:51:07 -0800

    Update Project

---
+++ IOS Water/Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ARStart::Start()
+extern void ARStart_Start_m5712D02A0DE3B679B2B77258E5D1C7027769CBC6 (void);
+// 0x00000002 System.Void ARStart::Update()
+extern void ARStart_Update_m4EA584F9EF7132DC0A850427F4152A248A163E02 (void);
+// 0x00000003 System.Void ARStart::OnSessionInitialized(Niantic.ARDK.AR.ARSessionEventArgs.AnyARSessionInitializedArgs)
+extern void ARStart_OnSessionInitialized_mFA1FFD63A961921E1762A7ACCB23AB17396C79F7 (void);
+// 0x00000004 System.Void ARStart::TouchBegan(UnityEngine.Touch)
+extern void ARStart_TouchBegan_mBE278BFA23983381819B5E89B8FDF51E14DADA83 (void);
+// 0x00000005 System.Void ARStart::.ctor()
+extern void ARStart__ctor_m319E4CC08DEFFB3B9C91ECA32EF5C0FFA5BFAE7A (void);
+// 0x00000006 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000007 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	ARStart_Start_m5712D02A0DE3B679B2B77258E5D1C7027769CBC6,
+	ARStart_Update_m4EA584F9EF7132DC0A850427F4152A248A163E02,
+	ARStart_OnSessionInitialized_mFA1FFD63A961921E1762A7ACCB23AB17396C79F7,
+	ARStart_TouchBegan_mBE278BFA23983381819B5E89B8FDF51E14DADA83,
+	ARStart__ctor_m319E4CC08DEFFB3B9C91ECA32EF5C0FFA5BFAE7A,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	5640,
+	5640,
+	4512,
+	4696,
+	5640,
+	5640,
+	5640,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
